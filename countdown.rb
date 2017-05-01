87482a013f0176b6fc193003cbe212fdb5453db0
Alexandria  Baranova <alexandrialmaloney@gmail.com>
2017-05-01 21:00:14 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
+def countdown_with_sleep(x)
+    sleep(x)
+  end
 
-def countdown
+def countdown(number)
+
+  while number > 0
+
+#    countdown_with_sleep(x)
+    puts "#{number} SECOND(S)!"
+              number-= 1
+    if number == 0
+      return 'HAPPY NEW YEAR!'
+      break;
+    end
+  end
 end
