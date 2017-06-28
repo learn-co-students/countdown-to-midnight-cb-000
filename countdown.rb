6197480a92c9a2aaa81f0f9cb488a75ab2f42cca
nikifi <nikifiona@gmail.com>
2017-06-28 00:17:21 -0400

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(arg)
+
+	while arg > 0
+		puts "#{arg} SECOND(S)!"
+		countdown_with_sleep(1)
+        arg = arg - 1   
+	end
+
+	"HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(seconds)
+
+	sleep(seconds)
+
+end
+
+
