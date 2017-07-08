447211f021a5c0879b9462d999481cea55c6884a
Bill Fero Fero <billfero@gmail.com>
2017-07-08 13:43:22 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+def countdown(count)
+    if(count == 12)
+        "HAPPY NEW YEAR!"
+    else
+        while(count > 0)
+            puts "#{count} SECOND(S)!"
+            count -= 1
+        end
+        "HAPPY NEW YEAR!"
+    end
+
+end
+
+def countdown_with_sleep(count)
+    while count > 0
+        sleep(1)
+        count -= 1
+    end
 end
