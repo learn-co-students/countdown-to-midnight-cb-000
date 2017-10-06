718668c4cdf117c7e0b2bbe043b5e1ea5db701e9
Ni Chia <nichia@gmail.com>
2017-10-06 18:09:54 +0000

    While loop to count down the clock

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+def countdown(number)
+    while number > 0
+      puts "#{number} SECOND(S)!"
+      number -= 1
+    end
+    puts "HAPPY NEW YEAR!"
 end
+
+countdown(5)
+
+def countdown_with_sleep(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -=1
+    sleep(1)
+  end
+  puts "HAPPY NEW YEAR"
+end
+countdown_with_sleep(5)
