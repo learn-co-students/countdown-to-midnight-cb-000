f057395cfb17b4ec612fa7fe0c215f82174fa607
Jeremy Parker <jrobinparker@gmail.com>
2018-06-08 16:33:21 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(number = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1])
+  
+  number = 10 
+  
+    while number < 10 
+      number -= 1
+      puts "#{number} SECOND(S)!"
+
+      if number = 0
+        puts "Happy New Year!"
+      end
+      
+    end  
+
+    
+  
 end
